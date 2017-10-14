2752fecce9739f01065b3e65c07cf3805130b8af
Luciano Oliveira <luciano101@msn.com>
2017-10-14 10:03:07 -0700

    finally got moment to work on karma

--- spec/age-spec.js
+++ spec/age-spec.js
@@ -14,8 +14,10 @@ describe('Age', function() {
   });
 
   it('should return the difference between today', () => {
-    let date = moment('2016-01-05');
-    expect(4).toEqual(reusableDate.getSecondsBetweenTwoDates(date));
+    let date = moment('2016-01-05').format('l');
+    console.log(reusableDate.getSecondsBetweenTwoDates);
+    console.log(date);
+    //expect(4).toEqual(reusableDate.getSecondsBetweenTwoDates(date));
   });
 
 
